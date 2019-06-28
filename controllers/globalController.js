36601b637ea470cbaa23ecbc3d05c2aaf0124a3f
sungkuk5420 <sungkuk5420@gmail.com>
2019-06-28 11:18:57 +0900

    Template Variables in Pug

--- controllers/globalController.js
+++ controllers/globalController.js
@@ -1,5 +1,5 @@
-export const home = (req, res) => res.render("home");
-export const search = (req, res) => res.send("Search");
-export const join = (req, res) => res.send("Join");
-export const login = (req, res) => res.send("Login");
-export const logout = (req, res) => res.send("Logout");
+export const home = (req, res) => res.render("home",{pageTitle:"main page"});
+export const search = (req, res) => res.send("Search",{pageTitle:"search page"});
+export const join = (req, res) => res.send("Join",{pageTitle:"join page"});
+export const login = (req, res) => res.send("Login",{pageTitle:"login page"});
+export const logout = (req, res) => res.send("Logout",{pageTitle:"logout page"});
