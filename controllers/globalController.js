f0f28cbe612a783cbf2aba9846df79b7443c2472
sungkuk5420 <sungkuk5420@gmail.com>
2019-06-28 12:42:59 +0900

    Search Controller

--- controllers/globalController.js
+++ controllers/globalController.js
@@ -1,5 +1,8 @@
 export const home = (req, res) => res.render("home",{pageTitle:"main page"});
-export const search = (req, res) => res.send("Search",{pageTitle:"search page"});
-export const join = (req, res) => res.send("Join",{pageTitle:"join page"});
-export const login = (req, res) => res.send("Login",{pageTitle:"login page"});
-export const logout = (req, res) => res.send("Logout",{pageTitle:"logout page"});
+export const search = (req, res) => {
+  const {query: {term:searchingBy} } = req;
+  res.render("search",{pageTitle:"search page",searchingBy});
+};
+export const join = (req, res) => res.render("join",{pageTitle:"join page"});
+export const login = (req, res) => res.render("login",{pageTitle:"login page"});
+export const logout = (req, res) => res.render("logout",{pageTitle:"logout page"});
