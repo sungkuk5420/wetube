bca20b00f36b2aff8eb25613a4e5ca35dd08fcab
sungkuk5420 <sungkuk5420@gmail.com>
2019-07-05 19:30:53 +0900

    Uploading and Creating a Video

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/uploads", express.static("uploads"));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
